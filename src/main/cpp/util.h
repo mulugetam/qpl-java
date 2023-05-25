1c8957698bfcf4cc4ff0e7cd8b06b226f1aab169
Mulugeta Mammo <mulugeta.mammo@intel.com>
2023-05-24 20:16:02 -0700

    Initial commit. Not sure if it compiles successfully.
    Signed-off-by: Mulugeta Mammo <mulugeta.mammo@intel.com>

--- src/main/cpp/util.h
+++ src/main/cpp/util.h
@@ -3,10 +3,15 @@
  *
  * SPDX-License-Identifier: MIT
  ******************************************************************************/
+#ifndef _Included_com_intel_qpl_util_h
+#define _Included_com_intel_qpl_util_h
 
 #include <jni.h>
+
 #include "qpl/qpl.h"
 
 void throw_exception(JNIEnv *env, const char *arg, jlong status);
 
 void throw_exception(JNIEnv *env, const char *arg);
+
+#endif
